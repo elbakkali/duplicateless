1b500bfd45b167953661aa2f311bd8a39715b61f
Adnane Elbakkali <adnane@elbakkali.me>
2020-06-14 21:12:59 +0200

    list all files in a dir recursively

---
+++ duplicateless.py
@@ -0,0 +1,26 @@
+import os
+
+def getListOfFiles(dirName):
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    for entry in listOfFile:
+        fullPath = os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles        
+
+def main():
+    dirName = 'D:\\Transend\\fthis\\Snapchat'
+    
+    listOfFiles = list()
+    for (dirpath, dirnames, filenames) in os.walk(dirName):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+        
+    for elem in listOfFiles:
+        print(elem)    
+        
+if __name__ == '__main__':
+    main()
