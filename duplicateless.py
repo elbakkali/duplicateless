fb0c257a8c04f7ec91fd1c9fc1bc4568ee2f294e
Adnane Elbakkali <adnane@elbakkali.me>
2020-06-21 19:07:38 +0200

    filter files by ext, sort, mark duplicates

--- duplicateless.py
+++ duplicateless.py
@@ -1,4 +1,6 @@
 import os
+import pprint
+import io
 
 def getListOfFiles(dirName):
     listOfFile = os.listdir(dirName)
@@ -13,14 +15,33 @@ def getListOfFiles(dirName):
     return allFiles        
 
 def main():
-    dirName = 'D:\\Transend\\fthis\\Snapchat'
+    dirName = 'D:\\'
     
     listOfFiles = list()
+
     for (dirpath, dirnames, filenames) in os.walk(dirName):
         listOfFiles += [os.path.join(dirpath, file) for file in filenames]
         
+    sortedDuplicates = {}
+
     for elem in listOfFiles:
-        print(elem)    
-        
+        if os.path.splitext(elem)[1] == '.jpg':
+            sortedDuplicates[elem] = os.path.basename(elem)
+
+    sorted_x = sorted(sortedDuplicates.items(), key=lambda kv: kv[1])
+    previousElem = ''
+    # fileObject = open('filesList.txt', 'w')
+    fileObject = io.open('filesList.txt', 'w', encoding="utf-8")
+    
+    for elem in sorted_x:
+        valueToAdd = elem[0]
+        if elem[1] == previousElem:
+            valueToAdd = '*********   ' + elem[0]
+        previousElem = elem[1]
+        fileObject.write(valueToAdd + '\n')
+
+    fileObject.close()
+    print('DOOOONE')
+
 if __name__ == '__main__':
     main()
